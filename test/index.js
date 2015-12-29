e414bd02830d692ffb6b6c61e7ba6375cc5a127d
Ionică Bizău <bizauionica@gmail.com>
2015-12-29 07:45:41 +0200

    Improved the regex
     - Use a function to not export the same reference to the regex but always return a new reference.
     - Improved the regex to handle + and - chars.
     - Better tests, using @muan's emojilib goodie. :grin:

--- test/index.js
+++ test/index.js
@@ -1,11 +1,20 @@
 const tester = require("tester")
     , match = require("match-all")
     , regexEmoji = require("../lib")
+    , emojis = require("emojilib").keys
     ;
 
 tester.describe("run the tests", test => {
     var s = "Hello _World_ and _Mars_";
     test.it("match all", () => {
-        test.expect(match(":house: sweet :house_with_garden:! :100: :clock1:", regexEmoji).toArray()).toEqual([ "house", "house_with_garden", "100", "clock1" ]);
+        test.expect(match(":house: sweet :house_with_garden:! :100: :clock1:", regexEmoji()).toArray()).toEqual([ "house", "house_with_garden", "100", "clock1" ]);
+    });
+    test.should("work with all emojis", () => {
+        emojis.forEach(c => {
+            test.expect(match(`:${c}:`, regexEmoji()).toArray()).toEqual([c]);
+        });
+    });
+    test.should("not be equal", () => {
+        test.expect(regexEmoji()).toNotBe(regexEmoji());
     });
 });
